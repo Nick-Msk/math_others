a8eb84c4f7103eb8d618b15fcc78f36f0ad8f0d4
Nick-Msk <vmksk.cur@gmail.com>
2025-11-25 14:02:39 +0300

    MATH: perelman task 91 changes (not done)

--- perelman/src/91task.c
+++ perelman/src/91task.c
@@ -9,12 +9,12 @@
 #include "check.h"
 #include "common.h"
 
-bool			gen_strings(char *buf, int cnt, int cnt_oper, int dec_value);
+bool			gen_strings(char *orig_buf, int orig_cnt, char *buf, int cnt, int cnt_oper, int dec_value);
 
 int				calc_string(const char *s, int elem);
 
 int				main(int argc, const char *argv[]){
-	loginit("91task.log", false, 0, "Start");
+	loginit("log/91task.log", false, 0, "Start");
 
 	int cnt_oper = 3;		// as per task
 	int des_value = 100;	// as per task
@@ -31,32 +31,39 @@ int				main(int argc, const char *argv[]){
 		return 1;
 	}
 
-	const int BUF_CNT = 10;
+	// main part
+	const int BUF_CNT = 9;
 	char buf[BUF_CNT * 2 + 1];
-	int res; 
 
-	buf[sizeof(buf) - 1] = '\0';
-	gen_strings(buf, BUF_CNT, cnt_oper, des_value);
+	// setup buffer
+	buf[BUF_CNT * 2 + 1] = '\0';
+	for (int i = 0; i < BUF_CNT; i++){
+		buf[2 *i] = ' '; 
+		buf[2 * i + 1] = i + 1 + '0';
+		//printf("i=%d, [%c][%c]\n", i, buf[i], buf[i + 1]);
+	}
+	printf("[%s]\n", buf);
 
-/*	if ((res = calc_string(buf, BUF_CNT)) == dec_value)
-		printf("%s = %d\n", buf, res);
-	else 
-		printf("%s = %d n*/
+	gen_strings(buf, BUF_CNT, buf, BUF_CNT, cnt_oper, des_value);
 
 	logclose("...");
 	return 0;
 }
 
 int				calc_string(const char *s, int elem){
+	logenter("%s[%d]", s, elem);
+	
 	int sum = 0, current = 0;
+	char op = ' '; // + or -
 	for (int i = 0; i < elem; i += 2){
 		switch (s[i]){
-			case '+':
-				sum += current;
-				current = 0;
-			break;
-			case '-':
-				sum -= current;
+			case '+': case '-':
+				// calculate last operation (if any)
+				if (op == '+')
+					sum += current;
+				else if (op == '-')
+					sum -= current;
+				op = s[i];
 				current = 0;
 			break;
 			case ' ':
@@ -64,13 +71,35 @@ int				calc_string(const char *s, int elem){
 			break;
 		}
 	}
-	return sum;
+	
+	if (op == '+')
+		sum += current;
+	else if (op == '-')
+		sum -= current;
+
+	return logret(sum, "%d", sum);
 }
 
 // TODO: gen_strings via recursion 
 
-bool			gen_strings(char *buf, int cnt, int cnt_oper, int dec_value){
+bool			gen_strings(char *orig_buf, int orig_cnt, char *buf, int cnt, int cnt_oper, int des_value){
+	logenter("buf [%s], cnt [%d], cnt_oper [%d]", buf, cnt, cnt_oper);	
+	int res;
 
-	return false;
+	if (cnt > 0){
+	//for (i = 0; i < cnt; i+= 2){
+			
+		// exec calc_string
+		if ((res = calc_string(orig_buf, orig_cnt)) == des_value)
+			printf("FOUND: %s = %d\n", orig_buf, res);
+		buf[1] = '+';
+		if ((res = calc_string(orig_buf, orig_cnt)) == des_value)
+			printf("FOUND: %s = %d\n", orig_buf, res);
+	    buf[1] = '-';
+		if ((res = calc_string(orig_buf, cnt)) == des_value)
+			printf("FOUND: %s = %d\n", orig_buf, res);
+		gen_strings(orig_buf, orig_cnt, buf + 2, cnt - 1, cnt_oper, des_value);
+	}
+	return logret(false, "...");
 }
 
