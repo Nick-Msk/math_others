1ce684ffb1ae75306a51394932e81dac2cc342c7
Nick-Msk <vmksk.cur@gmail.com>
2025-11-27 17:54:56 +0300

    MATH: task 4 (27 nov), interval checking is added

--- tasks/src/4task.c
+++ tasks/src/4task.c
@@ -15,8 +15,12 @@ typedef bool 	(*f_int2dim)(int, int, long);
 
 int				eng_int_2dim(int fromx, int fromy, int tox, int toy, long val, bool stoprun, f_int2dim target);
 
+int				check_interval(int fromx, int fromy, int tox, int toy, long val_from, long val_to, bool stoprun, f_int2dim target);
+
+
 int				main(int argc, const char *argv[]){
-	int 	val = 37;	// as per task
+	int 	val_curr = 37;	// as per task
+	int 	val_high;
 	int 	low, high;
 
 	if (!check_arg(3, "Usage: %s from to\n", *argv))
@@ -25,12 +29,17 @@ int				main(int argc, const char *argv[]){
 	low = atoi(argv[1]);
 	high = atoi(argv[2]);
 
+	if (argc > 3)
+		val_curr = atoi(argv[3]);
+
 	if (argc > 4)
-		val = atoi(argv[3]);
+		val_high = atoi(argv[4]);
+	else
+		val_high = val_curr;
 
-	printf("Start check value %d for interval [%d - %d]\n", val, low, high);
+	printf("Start check value %d-%d for interval [%d - %d]\n", val_curr, val_high, low, high);
 
-	int cnt = eng_int_2dim(low, low, high, high, (long)val, false, target_f);
+	int cnt = check_interval(low, low, high, high, (long)val_curr, (long)val_high, false, target_f);
 
 	if (cnt > 0)
 		printf("%d was/were found\n", cnt);
@@ -38,6 +47,13 @@ int				main(int argc, const char *argv[]){
 		printf("Not found for that interval...\n");
 
 	return 0;
+}
+
+int				check_interval(int fromx, int fromy, int tox, int toy, long val_from, long val_to, bool stoprun, f_int2dim target){
+	int total = 0;
+	for (long i = val_from; i <= val_to; i++)
+		total += eng_int_2dim(fromx, fromy, tox, toy, i, stoprun, target);
+	return total;
 }
 
 int				eng_int_2dim(int fromx, int fromy, int tox, int toy, long val, bool stoprun, f_int2dim target){
