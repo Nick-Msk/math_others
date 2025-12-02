93095bd318881027d6b7ede48bea80315cedb6b9
Nick-Msk <vmksk.cur@gmail.com>
2025-12-02 19:58:50 +0300

    MATH: 2 dim checking math, using eng_runner

--- tasks/src/7task.c
+++ tasks/src/7task.c
@@ -3,46 +3,38 @@
 #include <stdbool.h>
 
 #include "check.h"
-
-static int g_cnt = 0;
+#include "eng_runner.h"
 
 static  		bool target_f(int x, int y, long checked_val){
 	long res = (long) x * x * x * y - (long) y * y * y * x;
-	//static int cnt = 0;
-	g_cnt++;
-	if (g_cnt % 10000 == 0){
-	//	printf("x = %d, y = %d, res = %ld\n", x, y, res);
-		g_cnt = 0;
-	}
 	return res == checked_val;
 }
 
-static int 		g_eng_print_flag = 1;
-
-typedef bool 	(*f_int2dim)(int, int, long);
-
-// TODO: move to enging_test.h
-int				eng_int_2dim(int fromx, int fromy, int tox, int toy, long val, bool stoprun, f_int2dim target);
-
-// TODO: make a struct int_interval {}
-int				eng_check_int2dim_interval(int fromx, int fromy, int tox, int toy, long val_from, long val_to, bool stoprun, f_int2dim target);
 
 int			main(int argc, const char *argv[]){
-	if (!check_arg(3, "Usage: %s from to [value]\n", *argv))
+	if (!check_arg(3, "Usage: %s from to [min value (2023) - max value]\n", *argv))
         return 1;
 
-	int low, high;
-	low = atoi(argv[1]);
-	high = atoi(argv[2]);
-	
+	struct eng_int_interval ru1;
+	ru1.fromX = ru1.fromY = atoi(argv[1]);
+	ru1.toX = ru1.toY = atoi(argv[2]);
+	ru1.target = target_f;
+	ru1.printFlag = true;
+	ru1.stopRun = false;
+
 	int	val_curr = 2023;	// default as per task
+	int	val_to = val_curr;
 	if (argc > 3)
 		val_curr = atoi(argv[3]);
+	
+	if (argc > 4)
+		val_to = atoi(argv[4]);
 
-	printf("Check in integer x^3*y - x*y^3 = %d\nInterval from %d till %d\n", val_curr, val_curr, val_curr);
+	printf("Check in integer x^3*y - x*y^3 = %d - %d\nInterval from %d till %d\n", val_curr, val_to, ru1.fromX, ru1.toX);
 
 	// check_int_interval(struct int_interval *);
-	int cnt = eng_check_int2dim_interval(low, low, high, high, (long)val_curr, (long)val_curr, false, target_f);
+
+	int cnt = eng_check_int2dim_interval(ru1, (long)val_curr, (long)val_to);
 
 	if (cnt > 0)
 		printf("%d was/were found\n", cnt);
@@ -52,31 +44,4 @@ int			main(int argc, const char *argv[]){
 
 	return 0;
 }
-
-int				eng_check_int2dim_interval(int fromx, int fromy, int tox, int toy, long val_from, long val_to, bool stoprun, f_int2dim target){
-	int total = 0;
-	for (long i = val_from; i <= val_to; i++)
-		total += eng_int_2dim(fromx, fromy, tox, toy, i, stoprun, target);
-	return total;
-}
-
-int				eng_int_2dim(int fromx, int fromy, int tox, int toy, long val, bool stoprun, f_int2dim target){
-	int		total = 0;
-	// iterator need to be here
-	for (int x = fromx; x <= tox; x++)
-		for (int y = fromy; y <= toy; y++){
-			if (target(x, y, val)){
-				if (g_eng_print_flag)
-					printf("target function(%d, %d) is true for %ld)\n", x, y, val);
-				if (stoprun)
-					return 1;
-				else
-					total++;
-		}
-	}	
-	return total;
-
-}
-
-
 
