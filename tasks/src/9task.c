373a8814f74ddf15f63060687c5a46ad7895af35
Nick-Msk <vmksk.cur@gmail.com>
2025-12-09 18:52:33 +0300

    MATH: task 9 9 dec 2025

--- tasks/src/9task.c
+++ tasks/src/9task.c
@@ -10,12 +10,12 @@
 
 
 // n + 9 / n + 6
-static                  bool target_int_1dim_f(int x){
+static                  bool target_int_1dim_f(int n){
     return (n + 9) % (n + 6) == 0;
 }
 
 int                     main(int argc, const char *argv[]){
-    loginit("log/8task.log", false, 0, "Start");
+    loginit("log/9task.log", false, 0, "Start");
 
     if (!check_arg(3, "Usage: %s 'from' 'to' for the n + 9 / n + 6\n", *argv)){
     	return 1;
@@ -24,15 +24,15 @@ int                     main(int argc, const char *argv[]){
 	struct eng_int_interval	ru1;	// will use only toX, fromX
 	ru1.fromX = atoi(argv[1]);
 	ru1.toX = atoi(argv[2]);
-	ru1.targetValueFlag = true;	// bool function! 
+	ru1.targetValueFlag = false;	// bool function! 
 	ru1.stopRun = false;
 	ru1.printFlag = true;	// TODO: make a constructor!! to fill that stuff
 	ru1.modLog = 100;
-	ru1.f_int_1dim = target_int_1dim_f;
+	ru1.f_int_1dim_bool = target_int_1dim_f;
 
 	printf("Check n + 9 / n + 6 for natural for N in [%d - %d]\n", ru1.fromX, ru1.toX);
 	
-	int cnt = eng_int_1dim_bool(ru1);
+	int cnt = eng_int_1dim(ru1);
 
     if (cnt > 0){
         printf("%d was/were found\n", cnt);
