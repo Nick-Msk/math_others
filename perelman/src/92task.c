7beef93b8a626d15091bd0d6c39708853dd9615b
Nick-Msk <vmksk.cur@gmail.com>
2025-12-14 18:31:33 +0300

    MATH: perelman 92 (page 72) init

---
+++ perelman/src/92task.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "check.h"
+#include "eng_runner.h"
+#include "log.h"
+
+
+// x + y ==  x * u
+static                  bool target_flt_2dim_f(double x, double y){
+    return (x + y) == (x * 6);	// ?? DBL_EPSILON ?
+}
+
+int                     main(int argc, const char *argv[]){
+    loginit("log/92task.log", false, 0, "Start");
+
+    if (!check_arg(3, "Usage: %s 'from' 'to' for the x+y == x*y\n", *argv)){
+    	return 1;
+	}
+
+	struct eng_flt_interval	ru1;	// will use only toX, fromX
+	ru1.fromX = ru1.fromY = atof(argv[1]);
+	ru1.toX = ru1.toY = atof(argv[2]);
+	ru1.targetValueFlag = false;	// bool function! 
+	ru1.stopRun = false;
+	ru1.printFlag = true;	// TODO: make a constructor!! to fill that stuff
+	ru1.modLog = 10000;
+	ru1.f_flt_2dim_bool = target_flt_2dim_f;
+
+	printf("Check x+y == x*y for float in  [%g - %g]\n", ru1.fromX, ru1.toX);
+	
+	int cnt = eng_flg_2dim(ru1);
+
+    if (cnt > 0){
+        printf("%d was/were found\n", cnt);
+	}  else {
+        printf("Not found for that interval...\n");
+	}
+
+	logclose("...");
+	return 0;
+}
+
+
+
