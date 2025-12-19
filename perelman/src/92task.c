4382c81cc60e5873e96c56f076ff93db17d2cc79
Nick-Msk <vmksk.cur@gmail.com>
2025-12-19 20:03:19 +0300

    MATH: perelman task 92 (page 72) minor changes

--- perelman/src/92task.c
+++ perelman/src/92task.c
@@ -10,19 +10,24 @@
 
 // x + y ==  x * u
 static                  bool target_flt_2dim_f(double x, double y){
-    return (x + y) == (x * 6);	// ?? DBL_EPSILON ?
+    return (x + y) == (x * y);	// ?? DBL_EPSILON ?
 }
 
 int                     main(int argc, const char *argv[]){
     loginit("log/92task.log", false, 0, "Start");
 
-    if (!check_arg(3, "Usage: %s 'from' 'to' for the x+y == x*y\n", *argv)){
+    if (!check_arg(3, "Usage: %s 'from' 'to' 'step=0.000001' for the x+y == x*y\n", *argv)){
     	return 1;
 	}
 
 	struct eng_flt_interval	ru1;	// will use only toX, fromX
 	ru1.fromX = ru1.fromY = atof(argv[1]);
 	ru1.toX = ru1.toY = atof(argv[2]);
+	if (argc > 3)
+		ru1.stepX = atof(argv[3]);
+	else
+		ru1.stepX = 0.000001;	// default step
+								
 	ru1.targetValueFlag = false;	// bool function! 
 	ru1.stopRun = false;
 	ru1.printFlag = true;	// TODO: make a constructor!! to fill that stuff
