0ac6c442d190e90130fef8643f50a8678e5565c2
Nick-Msk <vmksk.cur@gmail.com>
2025-12-20 12:51:04 +0300

    MATH: perelman task 92 (page 72) minor changes 2

--- perelman/src/92task.c
+++ perelman/src/92task.c
@@ -2,21 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "check.h"
 #include "eng_runner.h"
 #include "log.h"
 
+static const double		EPS_VALUE = 1e-6;
 
 // x + y ==  x * u
-static                  bool target_flt_2dim_f(double x, double y){
-    return (x + y) == (x * y);	// ?? DBL_EPSILON ?
+static                  bool target_flt_2dim_f(double x, double y){ 
+	double v1 = (x + y);
+    double v2 = (x * y);	// ?? DBL_EPSILON ?
+	bool res = fabs(v1 - v2) < EPS_VALUE;	// should be refactored to avoid usage strange constant
+	if (res)
+		logsimple("x=%g, y=%g, * = %g, + = %g", x, y, v1, v2);
+	return res;
 }
 
 int                     main(int argc, const char *argv[]){
     loginit("log/92task.log", false, 0, "Start");
 
-    if (!check_arg(3, "Usage: %s 'from' 'to' 'step=0.000001' for the x+y == x*y\n", *argv)){
+    if (!check_arg(3, "Usage: %s 'from' 'to' 'step=0.001' for the x+y == x*y\n", *argv)){
     	return 1;
 	}
 
@@ -24,17 +31,17 @@ int                     main(int argc, const char *argv[]){
 	ru1.fromX = ru1.fromY = atof(argv[1]);
 	ru1.toX = ru1.toY = atof(argv[2]);
 	if (argc > 3)
-		ru1.stepX = atof(argv[3]);
+		ru1.stepX = ru1.stepY = atof(argv[3]);
 	else
-		ru1.stepX = 0.000001;	// default step
+		ru1.stepX = ru1.stepY = 0.001;	// default step
 								
 	ru1.targetValueFlag = false;	// bool function! 
 	ru1.stopRun = false;
 	ru1.printFlag = true;	// TODO: make a constructor!! to fill that stuff
-	ru1.modLog = 10000;
+	ru1.modLog = 100000;
 	ru1.f_flt_2dim_bool = target_flt_2dim_f;
 
-	printf("Check x+y == x*y for float in  [%g - %g]\n", ru1.fromX, ru1.toX);
+	printf("Check x+y == x*y for float in  [%g - %g]/%g\n", ru1.fromX, ru1.toX, ru1.stepX);
 	
 	int cnt = eng_flt_2dim(ru1);
 
