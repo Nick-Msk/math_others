2f0f9cb8994e04458a104843d777bdc76fb3f51b
Nick-Msk <vmksk.cur@gmail.com>
2026-01-20 14:30:46 +0300

    MATH: task from zen 20.01.2026 using eng_launcher INIT only

---
+++ tasks/src/15task_func.c
@@ -0,0 +1,28 @@
+#include "bool.h"
+#include "eng_launcher.h"
+
+// 1. target func
+bool f_eng_common(int x, int y){
+   return x + x * y + y == 7;
+}
+
+struct eng_launcher_data eng_data = (struct eng_launcher_data) {
+		.func_description = "x+xy+y==7", 
+		.elogfile =  "log/15_func.log",
+		.task_description = "task from zen 20.01.2025"
+};
+
+/*
+// 2. func desc
+const char              *eng_func_description = "x+xy+y==7";
+
+// 3. usage
+//const char 				*eng_usage = " conf_file, target";
+
+// 4. logfile 
+const char				*eng_logfile = "log/15_func.log"; // very dirty, log.h should be changed!! TODO:
+
+// 5. task description
+const char 				*eng_task_description = "task from zen 20.01.2025";
+*/
+
