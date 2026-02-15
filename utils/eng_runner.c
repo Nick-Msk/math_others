984f5db1a6624c983aa63141d6fb05dbfd239050
Nick-Msk <vmksk.cur@gmail.com>
2026-02-15 14:37:21 +0300

    UTILS: eng_runner: refactoring to use multy-dims

--- utils/eng_runner.c
+++ utils/eng_runner.c
@@ -11,13 +11,31 @@ double                  		g_eng_eps = FLT_EPSILON;    // for now
 
 
 #if defined(__clang__)
-	static const int			ENG_PREV_RES_CNT = 100;
-	static const int			ENG_MAXMSG 		 = 128; 
+	static const int			ENG_MAX_PROBE_PER_DIM 	= 16;
+	static const int			ENG_PREV_RES_CNT 		= 100;
+	static const int			ENG_MAXMSG 		 		= 256; 
 #else /* __GNUC__ */
-	#define                  	ENG_PREV_RES_CNT   100
-	#define						ENG_MAXMSG 		   128
+	#define                  	ENG_PREV_RES_CNT   		100
+	#define						ENG_MAXMSG 		   		256
+	#define						ENG_MAX_PROBE_PER_DIM 	16
 #endif
 
+// PROBE FUNCS
+// particular probe func
+typedef struct eng_probe {
+	int				dim;	// level
+	bool			(* func)(eng_int_interval v);	// point to probe wrapper   for ex for 2 dim - f(rt) = x1 + sqrt(x2);
+	char			text[ENG_MAXMSG];	// text of func with dims of %d, for ex for 2 dim - "%d + sqrt(%d)"
+} eng_probe;
+
+// TODO: iterate probe for all function on particular level
+typedef struct eng_dim_probe {
+	int				dim;	// dim for all probe functions
+	eng_probe		funcs[ENG_MAX_PROBE_PER_DIM];	// 16
+} eng_dim_probe;
+
+
+// ------------------------ RESULTSET ---------------------------------------------------
 struct eng_resultset {
 	double 	val;
 	char	msg[ENG_MAXMSG];
@@ -46,6 +64,7 @@ void							eng_check_reset(void){
 	pos_result = 0;
 }
 
+// ------------------------------- LOAD CONFIG (FROM FILE NOW, SHOULD BE FROM  SQLITE -------------------
 // interval
 static void						skip_line(FILE *f){
 	int c;
@@ -80,32 +99,33 @@ struct eng_int_interval        	eng_loadfromfile(const char *cfgname, bool stric
 		value = atol(val);
 		logmsg("name [%s], value [%ld], val [%s]", name, value, val);
 		// TODO: replace to macros
+		// for NOW only 4 dims are supported from config file
 		if (strcasecmp(name, "DIM") == 0)
 			st.useDim = value;	
 		else if (strcasecmp(name, "FROMX") == 0)
-			st.fromX = value;
+			st.dim[0].from = value;
 		else if (strcasecmp(name, "TOX") == 0)
-			st.toX = value;
+			st.dim[0].to = value;
 		else if (strcasecmp(name, "STEPX") == 0)
-			st.stepX = value;
+			st.dim[0].step = value;
 		else if (strcasecmp(name, "FROMY") == 0)
-			st.fromY = value;
+			st.dim[1].from = value;
 		else if (strcasecmp(name, "TOY") == 0)
-			st.toY = value;
+			st.dim[1].to = value;
 		else if (strcasecmp(name, "STEPY") == 0)
-			st.stepY = value;
+			st.dim[1].step = value;
 		else if (strcasecmp(name, "FROMZ") == 0)
-			st.fromZ = value;
+			st.dim[2].from = value;
 		else if (strcasecmp(name, "TOZ") == 0)
-			st.toZ = value;
+			st.dim[2].to = value;
 		else if (strcasecmp(name, "STEPZ") == 0)
-			st.stepZ = value;
+			st.dim[2].step = value;
 		else if (strcasecmp(name, "FROMZ1") == 0)
-			st.fromZ1 = value;
+			st.dim[3].from = value;
 		else if (strcasecmp(name, "TOZ1") == 0)
-			st.toZ1 = value;
+			st.dim[3].to = value;
 		else if (strcasecmp(name, "STEPZ1") == 0)
-			st.stepZ1 = value;
+			st.dim[3].step = value;
 		else if (strcasecmp(name, "MODLOG") == 0)
 			st.modLog = value;
 		else if (strcasecmp(name, "TARGETVALUE") == 0){
@@ -143,7 +163,10 @@ struct eng_int_interval        	eng_loadfromfile(const char *cfgname, bool stric
 	return logret(st, "... is_ok - %s", bool_str(eng_fl_error(st.flags)));
 }
 
+// -------------------------------- ENGINE --------------------------------------------
+//
 // utiversal wrapper for integer
+
 static bool						wrap_int(struct eng_int_interval rt){
 	bool					res = false;
 	int						dim = rt.useDim;
@@ -152,31 +175,79 @@ static bool						wrap_int(struct eng_int_interval rt){
 	switch (dim){
 		case 1:
 			if (targetValueFlag)
-				res = rt.f.f_int_1dim(rt.iterX, rt.targetValue);
+				res = rt.f.f_int_1dim(rt.dim[0].iter, rt.targetValue);
 			else
-				res = rt.f.f_int_1dim_bool(rt.iterX);
+				res = rt.f.f_int_1dim_bool(rt.dim[0].iter);
 		break;
 		case 2:	
 			if (targetValueFlag)
-				res = rt.f.f_int_2dim(rt.iterX, rt.iterY, rt.targetValue);
+				res = rt.f.f_int_2dim(rt.dim[0].iter, rt.dim[1].iter, rt.targetValue);
 			else
-				res = rt.f.f_int_2dim_bool(rt.iterX, rt.iterY);
+				res = rt.f.f_int_2dim_bool(rt.dim[0].iter, rt.dim[1].iter);
 		break;
 		case 3:
 			if (targetValueFlag)
-				res = rt.f.f_int_3dim(rt.iterX, rt.iterY, rt.iterZ, rt.targetValue);
+				res = rt.f.f_int_3dim(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, rt.targetValue);
 			else
-				res = rt.f.f_int_3dim_bool(rt.iterX, rt.iterY, rt.iterZ);
+				res = rt.f.f_int_3dim_bool(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter);
 		break;
 		case 4:
 			if (targetValueFlag)
-				res = rt.f.f_int_4dim(rt.iterX, rt.iterY, rt.iterZ, rt.iterZ1, rt.targetValue);
+				res = rt.f.f_int_4dim(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, rt.dim[3].iter, rt.targetValue);
 			else
-				res = rt.f.f_int_4dim_bool(rt.iterX, rt.iterY, rt.iterZ, rt.iterZ1);
+				res = rt.f.f_int_4dim_bool(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, rt.dim[3].iter);
+		break;
+		case 5:
+			if (targetValueFlag)
+				res = rt.f.f_int_5dim(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, rt.dim[3].iter, rt.dim[4].iter, rt.targetValue);
+			else
+				res = rt.f.f_int_5dim_bool(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, rt.dim[3].iter, rt.dim[4].iter);
+		break;
+		case 6:
+			if (targetValueFlag)
+				res = rt.f.f_int_6dim(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.targetValue);
+			else
+				res = rt.f.f_int_6dim_bool(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter);
+		break;
+		case 7:
+			if (targetValueFlag)
+				res = rt.f.f_int_7dim(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.dim[6].iter, rt.targetValue);
+			else
+				res = rt.f.f_int_7dim_bool(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.dim[6].iter);
+		break;
+		case 8:
+			if (targetValueFlag)
+				res = rt.f.f_int_8dim(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.dim[6].iter, rt.dim[7].iter, rt.targetValue);
+			else
+				res = rt.f.f_int_8dim_bool(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.dim[6].iter, rt.dim[7].iter);
+		break;
+		case 9:
+			if (targetValueFlag)
+				res = rt.f.f_int_9dim(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.dim[6].iter, rt.dim[7].iter, rt.dim[8].iter, rt.targetValue);
+			else
+				res = rt.f.f_int_9dim_bool(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.dim[6].iter, rt.dim[7].iter, rt.dim[8].iter);
+		break;
+		case 10:
+			if (targetValueFlag)
+				res = rt.f.f_int_10dim(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.dim[6].iter, 
+									rt.dim[7].iter, rt.dim[8].iter, rt.dim[9].iter, rt.targetValue);
+			else
+				res = rt.f.f_int_10dim_bool(rt.dim[0].iter, rt.dim[1].iter, rt.dim[2].iter, 
+									rt.dim[3].iter, rt.dim[4].iter, rt.dim[5].iter, rt.dim[6].iter, 
+									rt.dim[7].iter, rt.dim[8].iter, rt.dim[9].iter);
 		break;
 		default:
 			fprintf(stderr, "Unsupported DIM %d\n", dim);
-			// userraiseerr() TODO:
+			userraiseint(11, "Unsupported DIM %d\n", dim);
 		break;
 	}
 	return res;
@@ -184,13 +255,11 @@ static bool						wrap_int(struct eng_int_interval rt){
 
 // util, static
 static int						find_greatest(struct eng_int_interval rt){
-	int 	greatest = MAX(abs(rt.fromX), abs(rt.toX) );
-	if (rt.useDim > 1)
-		greatest = GREATEST(greatest, abs(rt.fromY), abs(rt.toY) );
-	if (rt.useDim > 2)
-		greatest = GREATEST(greatest, abs(rt.fromZ), abs(rt.toZ));
-	if (rt.useDim)
-		greatest = GREATEST(greatest, abs(rt.fromZ1), abs(rt.toZ1));
+	int 	greatest = 0;
+							
+	for (int i = 1; i <= rt.useDim; i++)
+		greatest = GREATEST(greatest, abs(rt.dim[i - 1].from), abs(rt.dim[i - 1].to) );	
+
 	return logautoret(greatest);  // logret(greatest, "gr: %d", greatest); // 
 }
 
@@ -203,70 +272,85 @@ int								eng_int_run(struct eng_int_interval rt){
 	bool					printFlag 		= eng_fl_print(rt.flags);
 	bool					stopRun 		= eng_fl_stopRun(rt.flags);
 	bool					targetValueFlag = eng_fl_targetValue(rt.flags);
-	int greatest = find_greatest(rt);
+	int 					greatest = find_greatest(rt);
+	int 					to[ENG_DIM_CAP - 1];
+	int 					from[ENG_DIM_CAP - 1];
 	logmsg("radius till %d", greatest);
 
-	if (rt.useDim < 2)
-		rt.fromY = rt.toY = 0;	// to make ONLY 1 cycle
-		
-	if (rt.useDim < 3)
-		rt.fromZ = rt.toZ = 0;	// to make ONLY 1 cycle
-	
-	if (rt.useDim < 4)
-		rt.fromZ1 = rt.toZ1 = 0;	// to make ONLY 1 cycle
+	// setup DIMS:
+	for (int i = 1; i <= ENG_DIM_CAP; i++){ // 1 till 10
+		if (i > rt.useDim)
+			rt.dim[i - 1].from = rt.dim[i - 1].to = 0;	// cleanup!
+	}
 
-	// iterator need to be here
-	for (int radius = 0; radius <= greatest; radius += rt.stepX){	// 1 step for ALL dums!
+
+	// iterator need to be here, go up to the radius
+	for (int radius = 0; radius <= greatest; radius += 1){	// 1 step for ALL dums!
 		if (rt.modLog > 0 && printFlag && cnt++ % rt.modLog == 0)
 			logsimple("cnt=%lu, radius %d, gr %d", cnt, radius, greatest);
+
+		for (int i = 1; i <= ENG_DIM_CAP; i++){ // 1 till 10
+			// setup limits
+			to[i] 	= MIN(radius, rt.dim[i - 1].to);
+			from[i] = MAX(-radius, rt.dim[i - 1].from);
+		}
+
+#define iterdim(d) rt.dim[d].iter = from[d]; rt.dim[d].iter <= to[d]; rt.dim[d].iter += rt.dim[d].step
+
 		// go throws dimensions																				
 		// iterator need to be here
-		//logmsg("fromx %d, tox %d radius %d", MAX(-radius, rt.fromX), MIN(radius, rt.toX), radius);
-		for (rt.iterX = MAX(-radius, rt.fromX); rt.iterX <= MIN(radius, rt.toX); rt.iterX += rt.stepX)
-		{
-			for (rt.iterY = MAX(-radius, rt.fromY); rt.iterY <= MIN(radius, rt.toY); rt.iterY += rt.stepY)
-			{
-				for (rt.iterZ = MAX(-radius, rt.fromZ); rt.iterZ <= MIN(radius, rt.toZ); rt.iterZ += rt.stepZ)
-				{
-					for (rt.iterZ1 = MAX(-radius, rt.fromZ1); rt.iterZ1 <= MIN(radius, rt.toZ1); rt.iterZ1 += rt.stepZ1)
-					{
-
-						if (rt.modLog > 0 && printFlag && cnt++ % rt.modLog == 0)
-							logsimple("cnt=%lu, x %d y %d z %d z1 %d", cnt, rt.iterX, rt.iterY, rt.iterZ, rt.iterZ1);
-						// exec wrapper here!
-						// for ALL dims
-						bool res = wrap_int(rt);
-						if (res){
-							if (printFlag){
-								logmsg("total %d, pos_result %d", total, pos_result);
-								if (total == pos_result - 1)
-									printf("%s\n", 	prev_results[pos_result - 1].msg);
-								if (strlen(rt.print_msg) > 0) { // that's a bit stupid, my understanding all messages must be via prev_result[].msg 
-									printf("Target function(%d, ", rt.iterX);
-									if (rt.useDim > 1)
-										printf("%d, ", rt.iterY);
-									if (rt.useDim > 2)
-										printf("%d, ", rt.iterZ);
-									if (rt.useDim > 3)
-										printf("%d ", rt.iterZ1);
-									printf(" is true ");
-								}
-								if (targetValueFlag)
-									printf("(for %ld)\n", rt.targetValue);
-								else
-									printf("\n");
-							}
-							if (stopRun)
-		    	            	return 1;
-        			   		else
-            		   			logauto(total++);
-						}
-					}
-				}
-			}
-		}
+		// DIM == 1
+		for (iterdim(0) )
+			// DIM == 2
+			for (iterdim(1))
+				for (iterdim(2))
+					for (iterdim(3))
+						for (iterdim(4))
+							for (iterdim(5))
+								for (iterdim(6))
+									for (iterdim(7))
+										for (iterdim(8))
+											for (iterdim(9))
+											{	// remove that logging after												
+												if (rt.modLog > 0 && printFlag && cnt++ % rt.modLog == 0)
+													logsimple("cnt=%lu, x0 %d x1 %d x2 %d x3 %d x4 %d x5 %d x6 %d x7 %d x8 %d x9 %d", cnt, 
+																		rt.dim[0].iter, 
+																		rt.dim[1].iter,
+																		rt.dim[2].iter, 
+																		rt.dim[3].iter, 
+																		rt.dim[4].iter, 
+																		rt.dim[5].iter, 
+																		rt.dim[6].iter,
+																		rt.dim[7].iter,
+																		rt.dim[8].iter,
+																		rt.dim[9].iter);
+												// exec wrapper here!
+												// for ALL dims
+												bool res = wrap_int(rt);
+												if (res){
+													if (printFlag){
+														// TODO:  refactor that!
+														logmsg("total %d, pos_result %d", total, pos_result);
+														if (total == pos_result - 1)
+															printf("%s\n", 	prev_results[pos_result - 1].msg);
+														if (strlen(rt.print_msg) > 0) { // that's a bit stupid, my understanding all messages must be via prev_result[].msg 
+															printf("Target function(");
+															for (int i = 0; i < ENG_DIM_CAP; i++)
+																printf("%d, ", rt.dim[i].iter);	// current iter pos
+															printf(") is true ");
+														}
+														if (targetValueFlag)
+															printf("(for %ld)\n", rt.targetValue);
+														else
+															printf("\n");
+													}
+													if (stopRun)
+		    	    					        		return 1;
+						        			   		else
+    	        							   			logauto(total++);
+												}
+											}
 	}	
-
 	return logret(total, "Total found %d", total);
 }
 
@@ -388,26 +472,17 @@ int                             eng_fautoprint(FILE *f, struct eng_int_interval 
 	cnt += fprintf(f, "%*cINT DIM=%d, targetValueFlag=%s, stopRun=%s, printFlag=%s, errorFlag=%s, modLog=%d, print_msg=%s\n",
 		f == logfile? logoffset : 0, '|',
 		v.useDim, bool_str(targetValueFlag), bool_str(stopRun), bool_str(printFlag), bool_str(errorFlag), v.modLog, v.print_msg);
-	
-	cnt += fprintf(f, "%*cfromX=%d, toX=%d, stepX=%d\n",
-		f == logfile? logoffset : 0, '|', v.fromX, v.toX, v.stepX);
-	
-	if (v.useDim > 1)
-		cnt += fprintf(f, "%*cfromY=%d, toY=%d, stepY=%d\n",
-			f == logfile? logoffset : 0, '|', v.fromY, v.toY, v.stepY);
-		
-	if (v.useDim > 2)
-		cnt += fprintf(f, "%*cfromZ=%d, toZ=%d, stepZ=%d\n",
-			f == logfile? logoffset : 0, '|', v.fromZ, v.toZ, v.stepZ);
 
-	if (v.useDim > 3)
-		cnt += fprintf(f, "%*cfromZ1=%d, toZ1=%d, stepZ1=%d\n",
-			f == logfile? logoffset : 0, '|', v.fromZ1, v.toZ1, v.stepZ1);
+	for (int i = 0; i < v.useDim; i++)
+		cnt += fprintf(f, "%*d: from=%d, to=%d, step=%d iter=%d\n",
+			f == logfile? logoffset : 0, i + 1, v.dim[i].from, v.dim[i].to, v.dim[i].step, v.dim[i].iter);
+	
 	cnt += fprintf(f, "%*cf_int%ddim%s=%p\n",
 		f == logfile? logoffset : 0, '|', v.useDim, targetValueFlag? "": "_bool", v.f.f_int_1dim /* anyway... */);
 	
 	if (targetValueFlag)
-		cnt += fprintf(f, "%*c targetValue=%ld\n", logoffset, '|', v.targetValue);
+		cnt += fprintf(f, "%*c targetValue=%ld\n", 
+		f == logfile? logoffset : 0, '|', v.targetValue);
 
 	return cnt;
 }
--- utils/eng_runner.h
+++ utils/eng_runner.h
@@ -5,24 +5,26 @@
 #include <stdio.h>
 #include <float.h>
 
-// as external
-extern 			union eng_un_int_func eng_un_int_func_api;
+#define							ENG_DIM_CAP 16
 
-static const 	int ENG_FLT_COMRARE_DEF_VALUE 	= FLT_EPSILON * 10;	// to use in target fuction to compart, not sure about value
-static const 	int ENG_PRINT_FLAG 				= 0x1;
-static const 	int ENG_STOP_RUN_FLAG			= 0x2;
-static const 	int ENG_TARGET_VALUE_FLAG		= 0x4;
-static const	int	ENG_ERROR_FLAG				= 0x8;
+// as external
+extern union eng_un_int_func 	eng_un_int_func_api;
+
+static const int 				ENG_FLT_COMRARE_DEF_VALUE 	= FLT_EPSILON * 10;	// to use in target fuction to compart, not sure about value
+static const int 				ENG_PRINT_FLAG 				= 0x1;
+static const int 				ENG_STOP_RUN_FLAG			= 0x2;
+static const int 				ENG_TARGET_VALUE_FLAG		= 0x4;
+static const int				ENG_ERROR_FLAG				= 0x8;
 
 // common
 typedef bool	(*tf_general)(bool);	// just for common run purpose
 
-// integer
-typedef bool    (*tf_int2dim)(int, int, long);
-typedef bool	(*tf_int2dim_bool)(int, int);	//  no long here!
-
+// integer prototypes
 typedef bool	(*tf_int1dim)(int, long);
 typedef bool	(*tf_int1dim_bool)(int);		// no long here!
+
+typedef bool    (*tf_int2dim)(int, int, long);
+typedef bool	(*tf_int2dim_bool)(int, int);	//  no long here!
 
 typedef bool    (*tf_int3dim)(int, int, int, long);
 typedef bool	(*tf_int3dim_bool)(int, int, int);	//  no long here!
@@ -30,6 +32,29 @@ typedef bool	(*tf_int3dim_bool)(int, int, int);	//  no long here!
 typedef bool    (*tf_int4dim)(int, int, int, int, long);
 typedef bool	(*tf_int4dim_bool)(int, int, int, int);	//  no long here!
 
+typedef bool    (*tf_int5dim)(int, int, int, int, int, long);
+typedef bool	(*tf_int5dim_bool)(int, int, int, int, int);	//  no long here!
+														//
+														//
+typedef bool    (*tf_int6dim)(int, int, int, int, int, int, long);
+typedef bool	(*tf_int6dim_bool)(int, int, int, int, int, int);	//  no long here!
+
+
+typedef bool    (*tf_int7dim)(int, int, int, int, int, int, int, long);
+typedef bool	(*tf_int7dim_bool)(int, int, int, int, int, int, int);	//  no long here!
+
+
+typedef bool    (*tf_int8dim)(int, int, int, int, int, int, int, int, long);
+typedef bool	(*tf_int8dim_bool)(int, int, int, int, int, int, int, int);	//  no long here!
+
+
+typedef bool    (*tf_int9dim)(int, int, int, int, int, int, int, int, int, long);
+typedef bool	(*tf_int9dim_bool)(int, int, int, int, int, int, int, int, int);	//  no long here!
+
+
+typedef bool    (*tf_int10dim)(int, int, int, int, int, int, int, int, int, int, long);
+typedef bool	(*tf_int10dim_bool)(int, int, int, int, int, int, int, int, int, int);	//  no long here!
+															
 // float
 typedef bool	(*tf_flt2dim)(double, double, double);
 typedef bool	(*tf_flt2dim_bool)(double, double);
@@ -39,46 +64,50 @@ typedef bool	(*tf_flt1dim_bool)(double);
 
 
 union eng_un_int_func {
-		tf_general		f_gen;
-		tf_int2dim		f_int_2dim;
-		tf_int2dim_bool	f_int_2dim_bool;
-		tf_int1dim		f_int_1dim;
-		tf_int1dim_bool	f_int_1dim_bool;
-		tf_int3dim		f_int_3dim;
-		tf_int3dim_bool	f_int_3dim_bool;
-		tf_int4dim		f_int_4dim;
-		tf_int4dim_bool	f_int_4dim_bool;
+		tf_general			f_gen;
+		tf_int1dim			f_int_1dim;
+		tf_int1dim_bool		f_int_1dim_bool;
+		tf_int2dim			f_int_2dim;
+		tf_int2dim_bool		f_int_2dim_bool;
+		tf_int3dim			f_int_3dim;
+		tf_int3dim_bool		f_int_3dim_bool;
+		tf_int4dim			f_int_4dim;
+		tf_int4dim_bool		f_int_4dim_bool;
+		tf_int5dim			f_int_5dim;
+		tf_int5dim_bool		f_int_5dim_bool;
+		tf_int6dim			f_int_6dim;
+		tf_int6dim_bool		f_int_6dim_bool;
+		tf_int7dim			f_int_7dim;
+		tf_int7dim_bool		f_int_7dim_bool;
+		tf_int8dim			f_int_8dim;
+		tf_int8dim_bool		f_int_8dim_bool;
+		tf_int9dim			f_int_9dim;
+		tf_int9dim_bool		f_int_9dim_bool;
+		tf_int10dim			f_int_10dim;
+		tf_int10dim_bool	f_int_10dim_bool;
 };
 
+typedef struct eng_int_dim {
+	int		from;
+	int		to;
+	int		step;
+	int		iter;
+} eng_int_dim;
+
 // integer structure
-struct eng_int_interval {
-	int			useDim;	// TODO!!!!!! 1, 2, 3, 4 for now
-	int			fromX;	// for now only 4 dims are supported
-	int			toX;
-	int			stepX;	// 1 by default
-	int			iterX;
-	int			fromY;
-	int			toY;
-	int			stepY;	// 1 by default
-	int			iterY;
-	int			fromZ;
-	int			toZ;
-	int			stepZ;	// 1 by default
-	int			iterZ;
-	int			fromZ1;
-	int			toZ1;
-	int			stepZ1;	// 1
-	int			iterZ1;
+typedef struct eng_int_interval {
+	int					useDim;	// now is limited to 10
+	eng_int_dim			dim[ENG_DIM_CAP - 1];	// 0..9
 	//------------------------------------------------------
 	int			flags;	// 0x1 , 0x2, 0x3, 0x4
 	long		targetValue;	// if NOT bool function
 	int			modLog;		// for printing logs cnt % modLog == 0
-	char 		print_msg[128];	// for primiting functional inertation
+	char 		print_msg[128];	// for primiting functional inertation TODO:remove, obsolete
 	union		eng_un_int_func f;
-};
+} eng_int_interval;
 
 
-struct eng_flt_interval {
+typedef struct eng_flt_interval {
 	double		fromX;	// for now only 2 dims are supported
 	double		toX;
 	double		stepX;	// add initStepX ? 
@@ -95,7 +124,7 @@ struct eng_flt_interval {
 		tf_flt1dim		f_flt_1dim;
 		tf_flt1dim_bool	f_flt_1dim_bool;
 	};
-};
+} eng_flt_interval;
 
 static bool						eng_fl_targetValue(int flags){
 	return flags & ENG_TARGET_VALUE_FLAG;
@@ -114,9 +143,12 @@ static bool						eng_fl_error(int flags){
 }
 
 // constructor (with filling f_gen()
-#define eng_create_int(...) (struct eng_int_interval)\
-{.useDim = 4, .stepX = 1, .stepY = 1, .stepZ = 1, .stepZ1 = 1,\
- .iterX = 0, .iterY = 0, .iterZ = 0, .iterZ1 = 0,\
+#define			eng_dim(...)(eng_int_dim){.from = 0, .to = 0, .step = 1, .iter = 0, __VA_ARGS__}
+#define			eng_init_dims(...)(eng_int_dim[]){__VA_ARGS__}
+
+#define 		eng_create_int(...) (struct eng_int_interval)\
+{.useDim = 4,\
+\
  .flags = ENG_PRINT_FLAG, .modLog = 0,\
  .f = eng_un_int_func_api, .print_msg="", __VA_ARGS__};	
 
@@ -127,11 +159,6 @@ struct eng_int_interval 		eng_loadfromfile(const char *cfgname, bool strict);
 // inteter
 // Common Integer runner!
 int								eng_int_run(struct eng_int_interval rt);
-/*
-int								eng_int_1dim(struct eng_int_interval rt);
-int                             eng_int_2dim(struct eng_int_interval rt);
-int								eng_int_3dim(struct eng_int_interval rt);
-int								eng_int_4dim(struct eng_int_interval rt); */
 
 // check to avoid the same result
 bool							eng_check_previous(double val, const char *msg, int sz);
